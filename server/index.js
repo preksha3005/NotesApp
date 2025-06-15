c30aa576b8cebec2a2e5cd4acda04b98befea8b8
preksha3005 <preksha.g504@gmail.com>
2025-06-15 16:31:58 +0530

    CORS change

--- server/index.js
+++ server/index.js
@@ -33,6 +33,8 @@ const __dirname = path.dirname(__filename);
 app.use(cors({
   origin: 'https://notesapp-frontend-jeyj.onrender.com',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 // app.use(
 //   cors({
