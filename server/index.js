6420569cb4dc680bfe18429181ab4299e51762ce
preksha3005 <preksha.g504@gmail.com>
2025-06-15 16:26:39 +0530

    CORS change

--- server/index.js
+++ server/index.js
@@ -31,7 +31,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(cors({
-  origin: 'https://notesapp-frontend-jeyj.onrender.com'
+  origin: 'https://notesapp-frontend-jeyj.onrender.com',
+  credentials: true,
 }));
 // app.use(
 //   cors({
